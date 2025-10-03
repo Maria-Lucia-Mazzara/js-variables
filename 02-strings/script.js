3b29acfd551f8df9ef14920bb4d2e3304d75585a
Maria-Lucia-Mazzara <marialuciaboolen@gmail.com>
2025-10-03 17:31:32 +0200

    fatto esercizio 1 e 2 documento 2

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -1,11 +1,20 @@
 // 1. Dichiarazione di variabili
 // Crea una variabile chiamata name e assegnale il tuo nome.
 // Stampa il valore di nome nella console.
+const name = 'Marilu'
+console.log(name)
 
 // 2. Tipi primitivi
 // Crea tre variabili chiamate number, text e bool e assegna rispettivamente
 // un numero, una stringa e un valore booleano.
 // Stampa tutti i valori nella console.
+let number = 30
+let text = 'ciao'
+let bool = true
+console.log(number);
+console.log(text);
+console.log(bool);
+
 
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente 
